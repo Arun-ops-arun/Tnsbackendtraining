57ebb244cd616e1f775af0aefb2331b808878784
Arun-ops-arun <godlxarun013@gmail.com>
2025-11-09 22:40:44 +0530

    Update ForLoopExample.java

--- ForLoopExample.java
+++ ForLoopExample.java
@@ -1,8 +1,9 @@
 public class ForLoopExample {
     public static void main(String[] args) {
-        // Print numbers from 1 to 10 using a for loop
-        for (int i = 1; i <= 10; i++) {
+        // Print numbers from 1 to 100 using a for loop
+        for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
     }
+
 }
